f7748481c8c44ca9fdc3c56eb0fd3550165d2b97
Malachi Nance <nancem1@mymail.nku.edu>
2024-10-01 10:31:24 -0400

    Added a sample organization to the json file

--- organizations.json.php
+++ organizations.json.php
@@ -1,6 +1,13 @@
 <?php
-die()
+die();
 ?>
 [
-    
+    {
+        "name":"Sample",
+        "goal":"Sample goal goes here",
+        "img":"SampleImg.src",
+        "datePublished":"10/1/2024",
+        "raisedFunds":8000,
+        "donationGoal":null
+    }
 ]
