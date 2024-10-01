e2331a29beb86bcd385f64b3ca226efc34c2c066
Malachi Nance <nancem1@mymail.nku.edu>
2024-10-01 10:35:08 -0400

    edited form

--- create.php
+++ create.php
@@ -11,6 +11,8 @@
             <input type="text" name="name" />
             <label>Goal</label>
             <input type="text" name="goal" />
+            <label>Set a Donation Goal (Optional)</label>
+            <input type="number" name="donationGoal" />
             <label>Upload Image</label>
             <input type="file" name="img" />
             <button type="submit">Submit</button>
