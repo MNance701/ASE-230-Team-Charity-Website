2a6804d0c170d0ab7fda73cb9697d49d37bb4712
Malachi Nance <nancem1@mymail.nku.edu>
2024-10-01 10:31:59 -0400

    Worked on the form

--- create.php
+++ create.php
@@ -1,6 +1,19 @@
+<?php
+?>
+
 <html>
     <head>
+        <title></title>
     </head>
     <body>
+        <form method="POST" enctype="multipart/form-data">
+            <label>Name</label>
+            <input type="text" name="name" />
+            <label>Goal</label>
+            <input type="text" name="goal" />
+            <label>Upload Image</label>
+            <input type="file" name="img" />
+            <button type="submit">Submit</button>
+        </form>
     </body>
 </html>
