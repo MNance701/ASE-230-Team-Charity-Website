b322766df389da63105b29a4172b4cadc5f03f58
Malachi Nance <nancem1@mymail.nku.edu>
2024-10-10 09:55:14 -0400

    Added auth.php

--- auth.php
+++ auth.php
@@ -1,3 +1,4 @@
 <?php
 //This file will contain functions that will help with authentication
 //Will be called by other pages to ensure that a user who is not logged cannot access certain pages
+session_start();
