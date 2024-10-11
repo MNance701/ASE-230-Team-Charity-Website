f72e10c1b98d951ecc7c8c22edbf0dcdb3c0d8b4
Malachi Nance <nancem1@mymail.nku.edu>
2024-10-11 12:31:16 -0400

    Added the charity's previous values to the form, and made the charity update when the form was submitted. Further testing still must be done before the page is finalized.

--- edit.php
+++ edit.php
@@ -1,20 +1,67 @@
+<?php
+//Gets the index of the specific charity
+$i=$_GET['index'];
+//If the submit button is pressed
+//The charity is changed
+if(count($_POST)>0){
+    //Remove <?php die()?\> from the json file to prevent reading errors
+    $string=file_get_contents('organizations.json.php');
+    $string=str_replace('<?php die()?>', '', $string);
+    //Read the json file
+    $charities=json_decode($string,true);
+    $raisedFunds=$charities[$i]['raisedFunds'];
+    
+    //for the image
+
+	if(isset($_FILES['image'])){
+		//the user wants to modify the image
+        $image=uniqid().'.'.pathinfo($_FILES['img']['name'])['extension'];
+	}else $image=$_POST['old_image'];
+    //Move from temp folder to the Logos folder
+    move_uploaded_file($_FILES['img']['tmp_name'],__DIR__.'/Logos/'.$filename);
+    //Store in db
+    $charity=[
+        'name'=>$_POST['name'],
+        'goal'=>$_POST['goal'],
+        'img'=>$image,
+        'datePublished'=>$_POST['datePublished'],
+        'raisedFunds'=>$raisedFunds,
+        'donationGoal'=>$_POST['donationGoal']
+    ];
+    //Add $charity to the $charities array
+    $charities[$i]=$charity;
+    //Write the json file with the <?php die()?\> and $charities
+    file_put_contents('organizations.json.php', '<?php die()?>'.json_encode($charities));
+}
+//If the charity form has not been submitted
+//Remove <?php die()?\> from the json file to prevent reading errors
+$string=file_get_contents('organizations.json.php');
+$string=str_replace('<?php die()?>', '', $string);
+//Read the json file
+$charities=json_decode($string,true);
+$oldCharity=$charities[$i];
+?>
 <html>
     <head>
-        <title></title>
+        <title>Edit</title>
     </head>
     <body>
         <form method="POST" enctype="multipart/form-data">
             <label>Name</label>
-            <input type="text" name="name" required />
+            <input type="text" name="name" value="<?= $oldCharity['name'] ?>" required />
             <label>Goal</label>
-            <input type="text" name="goal" required />
-            <label>Set a Donation Goal (Optional)</label>
-            <input type="number" name="donationGoal" />
-            <label>Upload Image</label>
-            <!--Made the image required for now-->
-            <input type="file" name="img" required />
+            <input type="text" name="goal" value="<?=$oldCharity['goal']?>" required />
+            <label>Date Founded</label>
+            <input type="date" name="date_founded" value="<?= $oldCharity['date_founded'] ?>" />
+            <label>Change Donation Goal</label>
+            <input type="number" name="donationGoal" value="<?=$oldCharity['donationGoal']?>" />
+            <label>Upload New Logo</label>
+            <!--Image is no longer required, since it is already set-->
+            <input type="file" name="image" />
+            <input type="hidden" name="old_image" value="<?= $oldCharity['img'] ?>" />            
             <button type="submit">Submit</button>
             <button type="reset">Reset Form</button>
         </form>
+        <a href="detail.php?index=<?=$i?>">Go Back</a>
     </body>
 </html>
