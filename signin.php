f940f7d5bd4ffe9ecfbcfc3fe7dbd9f99022e8db
gilla5 <156371739+gilla5@users.noreply.github.com>
2024-10-12 21:49:14 -0400

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,6 +1,15 @@
 <?php
 
 if(count($_POST)>0){
+	if(!isset($_POST['email'][0])) die('The email is required');
+	if(!isset($_POST['password'][0])) die('The password is required');
+	
+	if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) die('The email is in the wrong format');
+	
+	echo 'Thank you, '.htmlspecialchars($_POST['name']);
+}
+
+if(count($_POST)>0){
 	echo '<pre>';
 	print_r($_POST);
 	foreach($_POST as $key=>$value) echo $key.':'.$value.'<br />';
