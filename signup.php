02709b5852af4231bd38de8dc4066deb86115e0d
gilla5 <156371739+gilla5@users.noreply.github.com>
2024-10-12 21:59:34 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -13,6 +13,7 @@ if(count($_POST)>0){
 	
 	echo 'Thank you, '.htmlspecialchars($_POST['name']);
 }
+
 if(count($_POST)>0){
 	echo '<pre>';
 	print_r($_POST);
@@ -23,34 +24,42 @@ if(count($_POST)>0){
             <div> 
                 <label>First Name</label><br />
                 <input name="firstname" type="text" />
+                <span class="error">* required</span>
             </div>
             <div> 
                 <label>Last Name</label><br />
                 <input name="lastname" type="text" />
+                <span class="error">* required</span>
             </div>
             <div>
                 <label>Email</label><br />
                 <input name="email" type="email" />
+                <span class="error">* required</span>
             </div>
             <div> 
                 <label>Address</label><br />
                 <input name="address" type="text" />
+                <span class="error">* required</span>
             </div>
             <div>
                 <label>Age</label><br />
                 <input name="age" type="number" />
+                <span class="error">* required</span>
             </div>
             <div>
                 <label>Username</label><br />
                 <input name="username" type="text" />
+                <span class="error">* required</span>
             </div>
             <div> 
                 <label>Password</label><br />
                 <input name="password" type="password" />
+                <span class="error">* required</span>
             </div>
             <div> 
                 <label>Confirm Password</label><br />
                 <input name="confirmpassword" type="password" />
+                <span class="error">* required</span>
             </div>
             <button type="submit">Submit Form</button>
             <button type="Reset">Reset Form</button>
