f16569c9c1e4cce902d88fd957957f3a81035fa9
Malachi Nance <nancem1@mymail.nku.edu>
2024-10-12 22:42:10 -0400

    Changed bootstrap links to pull directly from bootstrap's website. Moved the for loop to encompass a whole box, so that each charity has its own box

--- index.php
+++ index.php
@@ -17,9 +17,7 @@ $charities=json_decode($string,true);
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Bootstrap icons-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/styles.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
     <body>
         <!-- Navigation-->
@@ -56,6 +54,7 @@ $charities=json_decode($string,true);
         <section class="py-5">
             <div class="container px-4 px-lg-5 mt-5">
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                <?php for($i=0;$i<count($charities);$i++){ /*start bracket for the for loop, so it can include multiple lines./*displayElement(echo $charities[$i]['name']/*)*/;?>
                     <div class="col mb-5">
                         <div class="card h-90">
                             <!-- Product details-->
@@ -63,12 +62,10 @@ $charities=json_decode($string,true);
                                 <div class="text-center">
                                     <!-- Product name-->
                                     <h5 class="fw-bolder">
-                                        <?php for($i=0;$i<count($charities);$i++){ /*start bracket for the for loop, so it can include multiple lines./*displayElement(echo $charities[$i]['name']/*)*/;?>
 										<div>
 											<h3><a href="detail.php?index=<?= $i ?>"><?= $charities[$i]['name'] ?>
 											<p><?= $charities[$i]['name']/*placeholder?*/ ?></p>
 										</div>
-                                        <?php /*ends the code that is in the for loop*/} ?>
 									</h5>
                                 </div>
                             </div>
@@ -78,6 +75,7 @@ $charities=json_decode($string,true);
                             </div>
                         </div>
                     </div>
+                <?php /*ends the code that is in the for loop*/} ?>
                     <div class="col mb-5">
                         <div class="card h-90">
                             <!-- Product details-->
@@ -211,8 +209,6 @@ $charities=json_decode($string,true);
             <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
         </footer>
         <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
 </html>
