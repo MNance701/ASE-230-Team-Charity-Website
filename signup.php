6fcec1f4647970fa0a3dd1760d6169d4521564ab
gilla5 <156371739+gilla5@users.noreply.github.com>
2024-10-12 21:47:58 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,6 +1,19 @@
 <?php
 
 if(count($_POST)>0){
+    if(!isset($_POST['firstname'][0])) die('The first name is required');
+	if(!isset($_POST['lastname'][0])) die('The last name is required');
+	if(!isset($_POST['email'][0])) die('The email is required');
+    if(!isset($_POST['address'][0])) die('The address is required');
+    if(!isset($_POST['age'][0])) die('The age is required');
+	if(!isset($_POST['password'][0])) die('The password is required');
+    if(!isset($_POST['confirmpassword'][0])) die('The confirm password is required');
+	
+	if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) die('The email is in the wrong format');
+	
+	echo 'Thank you, '.htmlspecialchars($_POST['name']);
+}
+if(count($_POST)>0){
 	echo '<pre>';
 	print_r($_POST);
 	foreach($_POST as $key=>$value) echo $key.':'.$value.'<br />';
