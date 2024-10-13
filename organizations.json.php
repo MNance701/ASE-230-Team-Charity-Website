32a40fdb395d9ef8612e58c6fad0ac11d4f4f366
Malachi Nance <nancem1@mymail.nku.edu>
2024-10-12 22:38:37 -0400

    Added second test charity for testing multiple charities

--- organizations.json.php
+++ organizations.json.php
@@ -7,5 +7,13 @@
         "datePublished":"10/1/2024",
         "raisedFunds":8000,
         "donationGoal":null
+    },
+    {
+        "name":"Sample",
+        "goal":"Sample goal goes here",
+        "img":"SampleImg.src",
+        "datePublished":"10/1/2024",
+        "raisedFunds":8000,
+        "donationGoal":null
     }
 ]
