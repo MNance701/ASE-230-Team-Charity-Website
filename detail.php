0564911eab86813f5eedb5d86afe906d7e3029c6
Malachi Nance <nancem1@mymail.nku.edu>
2024-10-12 22:12:47 -0400

    Added json reading code to detail.php, so that it can read the json file and put it into an array

--- detail.php
+++ detail.php
@@ -1,6 +1,10 @@
 <?php
 $i=$_GET['index'];
-
+//Remove <?php die()?\> from the json file to prevent reading errors
+$string=file_get_contents('organizations.json.php');
+$string=str_replace('<?php die()?>', '', $string);
+//Read the json file
+$charities=json_decode($string,true);
 ?>
 <html>
 	<head>
