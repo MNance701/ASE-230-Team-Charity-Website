0394bd802d22c0e1dd3c08d805b48d6a5180b411
Malachi Nance <nancem1@mymail.nku.edu>
2024-10-12 22:50:17 -0400

    Moved for loop to encompass a whole box

--- index.php
+++ index.php
@@ -49,17 +49,19 @@ $charities=json_decode($string,true);
         <section class="py-5">
             <div class="container px-4 px-lg-5 mt-5">
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                    <div class="col mb-5">
-                        <div class="card h-90">
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <h5 class="fw-bolder">
-                                        <?php for($i=0;$i<count($charities);$i++) /*displayElement(*/echo $charities[$i]['name']/*)*/;?>
-                                    </h5>
+                    <?php for($i=0;$i<count($charities);$i++) {?>
+                        <div class="col mb-5">
+                            <div class="card h-90">
+                                <div class="card-body p-4">
+                                    <div class="text-center">
+                                        <h5 class="fw-bolder">
+                                            <h3><a href="detail.php?index=<?= $i ?>"><?= $charities[$i]['name'] ?>
+                                        </h5>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>    
-                    </div>
+                            </div>    
+                        </div>
+                    <?php } ?>
                 </div>
             </div>
         </div>
