a135cb535a45da3bcbccbd3b1440991107194035
gilla5 <156371739+gilla5@users.noreply.github.com>
2024-12-09 23:06:52 -0500

    Update db.php

--- Database/db.php
+++ Database/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = '127.0.0.1';
 $db = 'charity_website';
-$user = 'root'; // Replace with your database username
-$pass = ''; // Replace with your database password
+$user = 'AMC';
+$pass = 'Charity2024$';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
