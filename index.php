8efb6835d5b5ff0e2c9d42377ab7fbe66170f457
cleytonshelton <156370909+cleytonshelton@users.noreply.github.com>
2024-12-10 16:45:52 -0500

    Update index.php
    added database to index

--- index.php
+++ index.php
@@ -5,7 +5,21 @@ $string=str_replace('<?php die()?>', '', $string);
 //Read the json file
 $charities=json_decode($string,true);
 ?>
+<?php
+$host = 'localhost';
+$db = 'charity_website';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
 
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+    $pdo = new PDO($dsn, $user, $pass, $options);
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
