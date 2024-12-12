5bc769ef42df334ddd8b1646c162fc0a3bf79895
Malachi Nance <nancem1@mymail.nku.edu>
2024-12-12 12:25:55 -0500

    Added bootstrap styling data

--- entities/campaigns/delete.php
+++ entities/campaigns/delete.php
@@ -18,7 +18,10 @@ if(isset($_GET['confirm']) && $_GET['confirm']==1){
 ?>
 <html>
     <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <title></title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
     <body>
         <h1>Are you sure you want to delete <?= $campaign['OrganizationName']?>?</h1>
@@ -26,5 +29,6 @@ if(isset($_GET['confirm']) && $_GET['confirm']==1){
         <h2>This will not affect any funds that have already been donated to the campaign</h2>
         <a href="delete.php?index=<?= $_GET['index'] ?>&confirm=1">Yes</a>
         <a href="detail.php?index=<?= $_GET['index'] ?>">No</a>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
 </html>
--- entities/charity_create_edit_delete/delete.php
+++ entities/charity_create_edit_delete/delete.php
@@ -22,12 +22,16 @@ if(isset($_GET['confirm']) && $_GET['confirm']==1){
 ?>
 <html>
     <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <title></title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
     <body>
         <h1>Are you sure you want to delete <?= $organization['OrganizationName']?>?</h1>
         <h2>This charity will no longer exist in the website, and the associated files, such as the logo, will be deleted from our systems</h2>
         <a href="delete.php?index=<?= $_GET['index'] ?>&confirm=1">Yes</a>
         <a href="../detail.php?index=<?= $_GET['index'] ?>">No</a>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
 </html>
